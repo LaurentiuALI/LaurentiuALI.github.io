910e14340a5b48e53976ffe4e48aed0abd2bb483
Andoni Laurentiu <iliuta.andoni01@e-uvt.ro>
2022-09-29 10:35:13 +0300

    changed implementantions for about me links

--- script.js
+++ script.js
@@ -2,18 +2,11 @@ var tablinks = document.getElementsByClassName("tab-links")
 var tabcontents = document.getElementsByClassName("tab-contents")
 
 function opentab(tabname){
-    const tab = document.getElementById(tabname);
-    
-    for(let i=0; i<tabcontents.length; i++){
-        if(tabcontents[i].id === tabname && !tabcontents[i].classList.contains('active-tab')){
-            tabcontents[i].classList.add("active-tab")
-            tablinks[i].classList.add("active-link")
-        }
-        else{
-            tabcontents[i].classList.remove("active-tab");
-            tablinks[i].classList.remove("active-link");
-        }
-    }
+    for(tablink of tablinks)
+        tablink.classList.remove("active-link");
+    for(tabcontent of tabcontents)
+        tabcontent.classList.remove("active-tab");
 
-
+    event.target.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
 }
